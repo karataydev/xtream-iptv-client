f8a56e8558858a2d78d59feb75a0ffe933acf3fc
karataymarufemre <karataymarufemre@gmail.com>
2024-09-14 00:57:17 +0300

    convert to static site

--- src/app/api/health/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-// To handle a GET request to /api
-export async function GET(request) {
-  // Do whatever you want
-  return NextResponse.json({ message: "OK" }, { status: 200 });
-}
